a9e5c6a3383c8273f3f23f8c04baeb730a357f51
Martynas <martynas.buivys@gmail.com>
2014-10-11 18:01:08 +0100

    Added dashboard stat directive

---
+++ ugracing_project/web/static/js/app/directives/dashboardStat.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('ugracing').directive('dashboardStat', function() {
+    return {
+        restrict: 'A',
+        templateUrl: 'static/views/directives/dashboard-stat.html'
+    };
+});
