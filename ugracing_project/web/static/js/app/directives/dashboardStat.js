7546c921df875358c4a2790d4fdbab168fb1699c
Martynas <martynas.buivys@gmail.com>
2014-10-11 18:07:28 +0100

    Modified directive scope bindings

--- ugracing_project/web/static/js/app/directives/dashboardStat.js
+++ ugracing_project/web/static/js/app/directives/dashboardStat.js
@@ -3,6 +3,10 @@
 angular.module('ugracing').directive('dashboardStat', function() {
     return {
         restrict: 'A',
+        scope: {
+        	colour: "@colour",
+        	icon: "@icon"
+        },
         templateUrl: 'static/views/directives/dashboard-stat.html'
     };
 });
