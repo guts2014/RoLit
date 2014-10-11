0627a1473277894e4198cebc783da37711d1350f
Martynas <martynas.buivys@gmail.com>
2014-10-11 21:53:49 +0100

    Updated api request function

--- ugracing_project/web/static/js/app/services/updateData.js
+++ ugracing_project/web/static/js/app/services/updateData.js
@@ -5,9 +5,12 @@ angular.module('ugracing').service('UpdateData', function($http, $q){
   var UpdateData = {};
 
   UpdateData.makeRequest = function(endpoint) {
-    return $http({
-      method: "POST",
-      url: endpoint
+    $http.get(endpoint).
+    success(function(data, status, headers, config) {
+      console.log(data);
+    }).
+    error(function(data, status, headers, config) {
+      console.log('Error: could not connect to API endpoint.');
     });
   };
 
