60daf63f2d0f1124eca88de980b821e2170feee7
Martynas <martynas.buivys@gmail.com>
2014-10-12 08:19:24 +0100

    Cleaned up the code

--- ugracing_project/web/static/js/app/controllers/dashboardController.js
+++ ugracing_project/web/static/js/app/controllers/dashboardController.js
@@ -44,51 +44,17 @@ angular.module('ugracing').controller('dashboardController', ['$scope', '$http',
 
 	// Keep updating the data forever
 	$interval(function() {
-
-	    $http.get('http://192.168.173.1:8000/telemetry-api/get-latest-values/').
-	    success(function(data, status, headers, config) {
-	   	  $scope.data = data;
-	   	  constructChartableDataset($scope.data);
-	   	  constructChartableDatasetArray();
-	    }).
-	    error(function(data, status, headers, config) {
-	      console.log('Error: could not connect to API endpoint.');
-	    });
-
+		//'telemetry-api/get-latest-values/'
+	    $http.get('http://192.168.173.1:8000/telemetry-api/get-latest-values/')
+	    		.success(function(data, status, headers, config) {
+	   	  			$scope.data = data;
+	   	  			constructChartableDataset($scope.data);
+	   	  			constructChartableDatasetArray();
+	    		})
+	    		.error(function(data, status, headers, config) {
+	      			console.log('Error: could not connect to API endpoint.');
+	    		});
 	}, 100);
-
-		var data = [],
-			totalPoints = 300;
-
-		function getRandomData() {
-
-			if (data.length > 0)
-				data = data.slice(1);
-
-			// Do a random walk
-
-			while (data.length < totalPoints) {
-
-				var prev = data.length > 0 ? data[data.length - 1] : 50,
-					y = prev + Math.random() * 10 - 5;
-
-				if (y < 0) {
-					y = 0;
-				} else if (y > 100) {
-					y = 100;
-				}
-
-				data.push(y);
-			}
-
-			// Zip the generated y values with the x values
-
-			var res = [];
-			for (var i = 0; i < data.length; ++i) {
-				res.push([i, data[i]])
-			}
-			return res;
-		}
 
 		// Set up the control widget
 
